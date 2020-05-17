7e8b7c0949767c6057f71c558950385cf7f7ce83
Sophie <sophieeqin@gmail.com>
2020-05-17 01:39:11 -0500

    displays title and artists of current playing song

--- Orpheus/Orpheus/PlayerViewController.swift
+++ Orpheus/Orpheus/PlayerViewController.swift
@@ -37,6 +37,8 @@ class PlayerViewController: NSViewController {
         self.view.addSubview(titleLabel)
         
         artistLabel.stringValue = currentTrack.artist
+        artistLabel.isEditable = false
+        artistLabel.isSelectable = false
         self.view.addSubview(artistLabel)
         
         applyConstraints()
@@ -46,6 +48,24 @@ class PlayerViewController: NSViewController {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         artistLabel.translatesAutoresizingMaskIntoConstraints = false
         
+        let titleLabelConstraints: [NSLayoutConstraint] = [
+            titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
+            titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+        ]
+        
+        let artistLabelConstraint: [NSLayoutConstraint] = [
+            artistLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            artistLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            artistLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+            artistLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50)
+        ]
+        
+        var constraints: [NSLayoutConstraint] = []
+        constraints += titleLabelConstraints
+        constraints += artistLabelConstraint
+        
+        NSLayoutConstraint.activate(constraints)
     }
     
     @objc func updateSongTrack(_ notification: NSNotification?) {
